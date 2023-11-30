2bb7201b8e009b340ef12220d368b839ac113ce0
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2023-11-29 21:47:50 -0800

    Completed creating Grid functionality.
    The function for creating grid works as expected populating the
    container div with square divs.

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ let grid = document.querySelector('.container');
 let numSquaresPerSide = 16;
 
 function createGrid(num) {
+    let squareLength = grid.clientWidth / num;
     for (let i = 0; i < num; i++) {
         for (let j = 0; j < num; j++) {
             let square = document.createElement('div');
             square.classList.add('square');
+            square.setAttribute('style', `width: ${squareLength}px; height: ${squareLength}px;`);
             grid.appendChild(square);
         }
     }
